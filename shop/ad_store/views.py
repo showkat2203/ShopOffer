ddd35a5366e4761e9ddb797ee7b06f76491ba812
showkat2203 <showkat2203@diu.edu.bd>
2019-10-28 02:45:12 +0600

    html and style pages location changed

--- shop/ad_store/views.py
+++ shop/ad_store/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
+
+    return render(request, 'index.html')
     return HttpResponse("Hello, world. You're at the polls index.")
 
