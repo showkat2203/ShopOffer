6ec36e577e304029c5d4f28cd9430f0b7dbbdc87
showkat2203 <showkat2203@diu.edu.bd>
2019-10-29 01:56:49 +0600

    added static files, restructured index.html

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
             ],
         },
     },
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'ad_store/static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
